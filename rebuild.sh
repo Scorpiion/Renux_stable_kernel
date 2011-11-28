d980b711180fb77081639431f73410f161c8b89d
Robert Åkerblom-Andersson <Robert.nr1@gmail.com>
2011-11-28 21:32:39 +0100

    Added function to build uImage.

--- rebuild.sh
+++ rebuild.sh
@@ -147,6 +147,15 @@ function make_zImage {
   cd ${DIR}/
 }
 
+function make_uImage {
+  cd ${DIR}/KERNEL/
+  echo "make -j${CORES} ARCH=arm LOCALVERSION=-${BUILD} CROSS_COMPILE=\"${CCACHE} ${CC}\" CONFIG_DEBUG_SECTION_MISMATCH=y uImage"
+  time make -j${CORES} ARCH=arm LOCALVERSION=-${BUILD} CROSS_COMPILE="${CCACHE} ${CC}" CONFIG_DEBUG_SECTION_MISMATCH=y uImage
+  KERNEL_UTS=$(cat ${DIR}/KERNEL/include/generated/utsrelease.h | awk '{print $3}' | sed 's/\"//g' )
+  cp arch/arm/boot/zImage ${DIR}/deploy/${KERNEL_UTS}.uImage
+  cd ${DIR}/
+}
+
 function make_modules {
   cd ${DIR}/KERNEL/
   time make -j${CORES} ARCH=arm LOCALVERSION=-${BUILD} CROSS_COMPILE="${CCACHE} ${CC}" CONFIG_DEBUG_SECTION_MISMATCH=y modules
@@ -191,6 +200,7 @@ if [ -e ${DIR}/system.sh ]; then
 #  copy_defconfig
   make_menuconfig
   make_zImage
+  make_uImage
   make_modules
 #  make_headers
 else
